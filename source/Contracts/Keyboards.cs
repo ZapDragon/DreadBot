cd8b9a562085d9595d2d73d6882da443267d08b6
ZapDragon <github@dragonstripes.net>
2020-05-11 04:52:08 -0700

    More Keyboard work
    Consolidation of Keyboard contacts.

--- source/Contracts/Keyboards.cs
+++ source/Contracts/Keyboards.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DreadBot
 {
-    static class KeyboardExtentions
+    public static class KeyboardExtentions
     {
         [Obsolete("setRowCount is unused. Please update your plugins to not use this method.")]
         public static void SetRowCount(this InlineKeyboardMarkup source, int x)
@@ -70,4 +71,82 @@ namespace DreadBot
             }
         }
     }
+
+
+    [DataContract]
+    public class ReplyKeyboardMarkup
+    {
+        [DataMember(Name = "keyboard")]
+        public KeyboardButton[][] keyboard { get; set; }
+
+        [DataMember(Name = "resize_keyboard", IsRequired = false)]
+        public bool resize_keyboard { get; set; }
+
+        [DataMember(Name = "one_time_keyboard", IsRequired = false)]
+        public bool one_time_keyboard { get; set; }
+
+        [DataMember(Name = "selective", IsRequired = false)]
+        public bool selective { get; set; }
+    }
+
+    [DataContract]
+    public class KeyboardButton
+    {
+        [DataMember(Name = "text")]
+        public string text { get; set; }
+
+        [DataMember(Name = "request_contact", IsRequired = false)]
+        public bool request_contact { get; set; }
+
+        [DataMember(Name = "request_location", IsRequired = false)]
+        public bool request_location { get; set; }
+    }
+
+    [DataContract]
+    public class ReplyKeyboardRemove
+    {
+        [DataMember(Name = "remove_keyboard")]
+        public bool remove_keyboard { get; set; }
+
+        [DataMember(Name = "selective", IsRequired = false)]
+        public bool selective { get; set; }
+    }
+
+    [DataContract]
+    public class InlineKeyboardMarkup
+    {
+        [DataMember(Name = "inline_keyboard")]
+        public List<List<InlineKeyboardButton>> inline_keyboard { get; set; } = new List<List<InlineKeyboardButton>>(100);
+    }
+
+    [DataContract]
+    public class InlineKeyboardButton
+    {
+        [DataMember(Name = "text")]
+        public string text { get; set; }
+
+        [DataMember(Name = "url", EmitDefaultValue = false, IsRequired = false)]
+        public string url { get; set; }
+
+        [DataMember(Name = "login_url", EmitDefaultValue = false, IsRequired = false)]
+        public LoginUrl login_url { get; set; }
+
+        [DataMember(Name = "callback_data", EmitDefaultValue = false, IsRequired = false)]
+        public string callback_data { get; set; }
+
+        [DataMember(Name = "switch_inline_query", EmitDefaultValue = false, IsRequired = false)]
+        public string switch_inline_query { get; set; }
+
+        [DataMember(Name = "switch_inline_query_current_chat", EmitDefaultValue = false, IsRequired = false)]
+        public string switch_inline_query_current_chat { get; set; }
+
+        [DataMember(Name = "callback_game", EmitDefaultValue = false, IsRequired = false)]
+        public CallbackGame callback_game { get; set; }
+
+        [DataMember(Name = "pay", EmitDefaultValue = false, IsRequired = false)]
+        public bool pay { get; set; }
+    }
+
+
+
 }
